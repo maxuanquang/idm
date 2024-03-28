b09b86d4e719b8e29d6adc2f479f219c2f76f022
maxuanquang <maxuanquang@gmail.com>
2024-03-28 20:54:49 +0700

    Implement download to s3 client

--- go.mod
+++ go.mod
@@ -17,10 +17,12 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/eapache/go-resiliency v1.6.0 // indirect
 	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
 	github.com/eapache/queue v1.1.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
@@ -32,17 +34,28 @@ require (
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/minio/minio-go/v7 v7.0.69 // indirect
+	github.com/minio/sha256-simd v1.0.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/rs/xid v1.5.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 )
 
 require (
 	github.com/IBM/sarama v1.43.0
+	github.com/aws/aws-sdk-go v1.51.9
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/go-sql-driver/mysql v1.8.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
--- internal/dataaccess/file/client.go
+++ internal/dataaccess/file/client.go
@@ -10,6 +10,8 @@ import (
 
 	"github.com/maxuanquang/idm/internal/configs"
 	"github.com/maxuanquang/idm/internal/utils"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"go.uber.org/zap"
 )
 
@@ -75,7 +77,128 @@ func (l *localClient) Write(ctx context.Context, fileName string) (io.WriteClose
 	return file, nil
 }
 
-// TODO: Implement this
 func NewS3Client(downloadConfig configs.Download, logger *zap.Logger) (Client, error) {
-	return nil, nil
+
+	minioClient, err := minio.New(
+		downloadConfig.Address,
+		&minio.Options{
+			Creds: credentials.NewStaticV4(
+				downloadConfig.Username,
+				downloadConfig.Password,
+				"",
+			),
+		},
+	)
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to create minio client")
+		return nil, err
+	}
+
+	// Make a new bucket
+	bucketName := downloadConfig.Bucket
+	err = minioClient.MakeBucket(context.Background(), bucketName, minio.MakeBucketOptions{})
+	if err != nil {
+		// Check to see if we already own this bucket (which happens if you run this twice)
+		exists, errBucketExists := minioClient.BucketExists(context.Background(), bucketName)
+		if errBucketExists == nil && exists {
+			logger.Info("bucket existed")
+		} else {
+			logger.With(zap.Error(err)).Error("failed to create bucket")
+			return nil, err
+		}
+	}
+
+	return &s3Client{
+		minioClient: minioClient,
+		logger:      logger,
+		bucketName:  bucketName,
+	}, nil
+}
+
+type s3Client struct {
+	minioClient *minio.Client
+	logger      *zap.Logger
+	bucketName  string
+}
+
+// Read implements Client.
+func (s *s3Client) Read(ctx context.Context, fileName string) (io.ReadCloser, error) {
+	logger := utils.LoggerWithContext(ctx, s.logger).With(zap.String("file_name", fileName))
+
+	object, err := s.minioClient.GetObject(ctx, s.bucketName, fileName, minio.GetObjectOptions{})
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to get object")
+		return nil, err
+	}
+
+	return object, nil
+}
+
+// Write implements Client.
+func (s *s3Client) Write(ctx context.Context, fileName string) (io.WriteCloser, error) {
+	return newS3ReadWriteCloser(
+		ctx,
+		s.minioClient,
+		s.logger,
+		s.bucketName,
+		fileName,
+	)
+}
+
+func newS3ReadWriteCloser(
+	ctx context.Context,
+	minioClient *minio.Client,
+	logger *zap.Logger,
+	bucketName string,
+	fileName string,
+) (*s3ReadWriteCloser, error) {
+	logger = utils.LoggerWithContext(ctx, logger)
+	readWriteCloser := &s3ReadWriteCloser{
+		buffer:   make([]byte, 0),
+		isClosed: false,
+	}
+
+	go func() {
+		if _, err := minioClient.PutObject(
+			ctx,
+			bucketName,
+			fileName,
+			readWriteCloser,
+			-1,
+			minio.PutObjectOptions{},
+		); err != nil {
+			logger.With(zap.Error(err)).Error("failed to put object")
+		}
+	}()
+
+	return readWriteCloser, nil
+}
+
+type s3ReadWriteCloser struct {
+	buffer   []byte
+	isClosed bool
+}
+
+func (s *s3ReadWriteCloser) Read(p []byte) (int, error) {
+	if len(s.buffer) > 0 {
+		readLength := copy(p, s.buffer)
+		s.buffer = s.buffer[readLength:]
+		return readLength, nil
+	}
+
+	if s.isClosed {
+		return 0, io.EOF
+	}
+
+	return 0, nil
+}
+
+func (s *s3ReadWriteCloser) Write(p []byte) (int, error) {
+	s.buffer = append(s.buffer, p...)
+	return len(p), nil
+}
+
+func (s *s3ReadWriteCloser) Close() error {
+	s.isClosed = true
+	return nil
 }
